e4512cf8030ccb8abaa832e4556ae6fdd778913a
vPablo15 <pablo.valero25@gmail.com>
2021-06-23 00:50:32 -0300

    Co-authored-by: 02Nico02 <02Nico02@users.noreply.github.com> Co-authored-by: emanuelbanega <emanuelbanega@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import ReactDOM from "react-dom";
 import { Combi19App } from "./Combi19App.js";
 
 ReactDOM.render(<Combi19App />, document.getElementById("root"));
+
+//Este comentario es de prueba
